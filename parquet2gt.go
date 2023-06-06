0564b34c2a2444d5159aa97051d1384aee5da66a
yuanbohan <yuanbo.han@gmail.com>
2023-06-06 15:26:22 +0800

    fix: panic when stream insert (#1)

--- parquet2gt.go
+++ parquet2gt.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"math/rand"
 	"time"
 
 	gt "github.com/GreptimeTeam/greptimedb-client-go"
@@ -13,12 +15,24 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+var (
+	options = []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	cfg = gt.NewCfg("127.0.0.1").
+		WithPort(4001).         // default is 4001.
+		WithDatabase("public"). // specify your database
+		WithAuth("", "").
+		WithDialOptions(options...). // specify your gRPC dail options
+		WithCallOptions()            // specify your gRPC call options
+)
+
 type Row struct {
 	Lts  time.Time `parquet:"lts,timestamp(microsecond)"`
 	Rts  time.Time `parquet:"rts,timestamp(microsecond)"`
 	Px   float64   `parquet:"px"`
 	Qty  float64   `parquet:"qty"`
-	Flag []uint8   `parquet:"flag"`
+	Flag []byte    `parquet:"flag"`
 }
 
 func main() {
@@ -50,46 +64,104 @@ func main() {
 	schema := parquet.SchemaOf(new(Row))
 	log.Info(schema)
 
-	options := []grpc.DialOption{
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	}
-	cfg := gt.NewCfg("127.0.0.1").
-		WithPort(4001).         // default is 4001.
-		WithDatabase("public"). // specify your database
-		WithAuth("", "").
-		WithDialOptions(options...). // specify your gRPC dail options
-		WithCallOptions()            // specify your gRPC call options
+	// insert(rows)
+	streamInsert(rows)
+}
 
-	client, err := gt.NewStreamClient(cfg)
+func doInsert(client *gt.Client, table string, metric gt.Metric) {
+	req := gt.InsertRequest{}
+	req.WithTable(table).WithMetric(metric)
+	reqs := gt.InsertsRequest{}
+	reqs.Insert(req)
+
+	fmt.Printf("ready to insert %d series\n", len(metric.GetSeries()))
+	_, err := client.Insert(context.Background(), reqs)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insert(rows []Row) {
+	client, err := gt.NewClient(cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	reqs := gt.InsertsRequest{}
 	metric := gt.Metric{}
 	metric.SetTimestampAlias("lts")
 	metric.SetTimePrecision(time.Microsecond)
+
+	table := fmt.Sprintf("trade_%d", rand.Intn(100000))
+	fmt.Printf("table: %s\n", table)
+
 	for i, c := range rows {
-		log.Debug(c)
+		// log.Debug(c)
 
 		series := gt.Series{}
 		series.SetTimestamp(c.Lts)
 		series.AddField("rts", c.Rts)
 		series.AddField("px", c.Px)
 		series.AddField("qty", c.Qty)
-		series.AddField("flag", c.Flag)
+		series.AddField("flag", string(c.Flag))
 		metric.AddSeries(series)
-		req := gt.InsertRequest{}
-		req.WithTable("trade2").WithMetric(metric)
-		reqs.Insert(req)
 
-		if i%100 == 0 {
-			if err = client.Send(context.Background(), reqs); err != nil {
-				panic(err)
-			}
+		if i > 0 && i%5000 == 0 {
+			doInsert(client, table, metric)
+			metric = gt.Metric{}
+			metric.SetTimestampAlias("lts")
+			metric.SetTimePrecision(time.Microsecond)
 		}
-
 	}
+
+	doInsert(client, table, metric)
+}
+
+func doStreamInsert(client *gt.StreamClient, table string, metric gt.Metric) {
+	req := gt.InsertRequest{}
+	req.WithTable(table).WithMetric(metric)
+	reqs := gt.InsertsRequest{}
+	reqs.Insert(req)
+
+	fmt.Printf("ready to insert %d series\n", len(metric.GetSeries()))
+	if err := client.Send(context.Background(), reqs); err != nil {
+		fmt.Printf("err: %v\n", err)
+		panic(err)
+	}
+}
+
+func streamInsert(rows []Row) {
+	client, err := gt.NewStreamClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	metric := gt.Metric{}
+	metric.SetTimestampAlias("lts")
+	metric.SetTimePrecision(time.Microsecond)
+
+	table := fmt.Sprintf("trade_%d", rand.Intn(100000))
+	fmt.Printf("table: %s\n", table)
+
+	for i, c := range rows {
+		// log.Debug(c)
+
+		series := gt.Series{}
+		series.SetTimestamp(c.Lts)
+		series.AddField("rts", c.Rts)
+		series.AddField("px", c.Px)
+		series.AddField("qty", c.Qty)
+		series.AddField("flag", string(c.Flag))
+		metric.AddSeries(series)
+
+		if i > 0 && i%5000 == 0 {
+			doStreamInsert(client, table, metric)
+			metric = gt.Metric{}
+			metric.SetTimestampAlias("lts")
+			metric.SetTimePrecision(time.Microsecond)
+		}
+	}
+	doStreamInsert(client, table, metric)
+
 	_, err = client.CloseAndRecv(context.Background())
 	if err != nil {
 		panic(err)
